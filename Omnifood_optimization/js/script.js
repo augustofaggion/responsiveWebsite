56ef442d3a9e5547eb907b52d732fb35725c92de
Augusto Faggion <afaggionliradossantos@gmail.com>
2023-12-31 18:52:26 +0100

    scroll function

--- Omnifood_optimization/js/script.js
+++ Omnifood_optimization/js/script.js
@@ -22,6 +22,35 @@ btnNavEl.addEventListener("click", function () {
   headerEL.classList.toggle("nav-open");
 });
 
+/* Safari scrolling animation*/
+// const sectionEl = document.querySelectorAll("section");
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    /*Scroll back to top */
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    /* close nav */
+    if (link.classList.contains("main-nav-link"))
+      headerEL.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 // function checkFlexGap() {
